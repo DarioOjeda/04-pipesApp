18752ed995bcc4434bb9179511c32ac0fcea8f63
CabesaDario <57437897+CabesaDario@users.noreply.github.com>
2021-07-11 12:31:29 +0200

    primeNg flex usado y pipes de capitalización

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
 import { SharedModule } from './shared/shared.module';
 import { AppRouterModule } from './app-router.module';
+import { VentasModule } from './ventas/ventas.module';
 
 
 @NgModule({
@@ -13,7 +15,8 @@ import { AppRouterModule } from './app-router.module';
   imports: [
     AppRouterModule,
     BrowserModule,
-    SharedModule
+    SharedModule,
+    VentasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
