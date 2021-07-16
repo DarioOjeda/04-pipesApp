2a70f7aff9da3c9cb5429dbd60dc74e90a9f4332
DarioOjeda <darioojeda_1995@hotmail.com>
2021-07-16 14:01:47 +0200

    pipe para cambiar el orden con distintas propiedades
    y botones para cambiar lo que recibe el pipe por argumento(propiedad del ordenar component).

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -11,6 +11,8 @@ import { ColorPrin, Heroe } from '../../interfaces/ventas.interfaces';
 export class OrdenarComponent{
 
   enMayusculas: boolean = true;
+
+  ordenarPor: string = '';
   heroes: Heroe[] = [
     {
       nombre: 'Superman',
@@ -36,6 +38,10 @@ export class OrdenarComponent{
 
   cambiarMayus(): void {
     this.enMayusculas = !this.enMayusculas;
+  }
+
+  cambiarOrden( valor: string ){
+    this.ordenarPor = valor;
   }
 
 }
